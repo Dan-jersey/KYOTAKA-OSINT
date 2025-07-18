c694fa39a7685b48fd73782bbbe38d47858f5517
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-07-19 00:16:36 +0200

    Create kyotaka_osint.py

---
+++ kyotaka_osint.py
@@ -0,0 +1,11 @@
+# kyotaka_osint.py
+from modules import ip_lookup
+
+def main():
+    print("┌─[ KYOTAKA OSINT ]")
+    print("│ Outil OSINT sombre et rapide")
+    print("└──────────────────────────────")
+    ip_lookup.run()
+
+if __name__ == "__main__":
+    main()
